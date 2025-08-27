83099e5e00a58784bf815d89f1d3ceb9f7a71cab
Pratik Shelke <115389960+gitpratikshelke@users.noreply.github.com>
2025-08-27 18:49:59 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull pratik83/tws-online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 pratik83/tws-online-shop-app-demo:latest
